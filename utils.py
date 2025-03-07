34ee401df08628d565d8ea10a10c6bab6979ad62
hw@chill <hw56@indiana.edu>
2025-03-07 01:45:03 -0500

    feat: add missing dependency

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import torch
+from itertools import starmap
+
 
 
 from transformers import PatchTSTConfig, PatchTSTForClassification, Trainer, TrainingArguments
